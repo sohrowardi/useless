5507904856f8467327fa4df6b495451caa80e5b5
Mohammad Sohrowardi <sohrowardi99@gmail.com>
2024-06-04 04:59:55 +0600

    deleted:    Get-Folder-Structure-With-Content/txt/bats.txt

--- Rename Thumb/Rename_Thumb.py
+++ Rename Thumb/Rename_Thumb.py
@@ -3,22 +3,24 @@ import re
 
 def rename_files(directory):
     for root, dirs, files in os.walk(directory):
+        movie_files = {}  # Dictionary to keep track of counts for each movie name
+
         for filename in files:
             if filename.endswith(".png"):
-                # Extract the movie name from the filename
-                movie_name = re.search(r'(.+?)(\d{4}-\d{2}-\d{2}-\d{2}h\d{2}m\d{2}s\d{3})', filename).group(1)
-                
-                # Remove the file extension from the movie name
-                movie_name = os.path.splitext(movie_name)[0]
-                
-                # Count the number of files with the same movie name
-                same_movie_files = [file for file in os.listdir(root) if file.startswith(movie_name)]
-                file_number = same_movie_files.index(filename) + 1
-                
-                # Create the new filename
-                new_filename = f"{movie_name} ({file_number}).png"
-                
-                # Rename the file
-                os.rename(os.path.join(root, filename), os.path.join(root, new_filename))
+                match = re.search(r'(.+?)(\d{4}-\d{2}-\d{2}-\d{2}h\d{2}m\d{2}s\d{3})', filename)
+                if match:
+                    movie_name = match.group(1).strip()
+
+                    # Update the count for the movie name
+                    if movie_name not in movie_files:
+                        movie_files[movie_name] = 0
+                    movie_files[movie_name] += 1
+                    file_number = movie_files[movie_name]
+
+                    # Create the new filename
+                    new_filename = f"{movie_name} ({file_number}).png"
+
+                    # Rename the file
+                    os.rename(os.path.join(root, filename), os.path.join(root, new_filename))
 
 rename_files(os.getcwd())
